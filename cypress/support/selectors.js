b7c31ed89073f54d68c0c4ac308205ac7dafd967
lucasfernandez789 <4518625@gmail.com>
2025-10-29 12:49:23 -0300

    Clean: eliminar archivos innecesarios de rama gh-pages, mantener solo archivos de producción

--- cypress/support/selectors.js
+++
@@ -1,5 +0,0 @@
-export const inputUsername = 'input[name="username"]';
-export const inputPassword = 'input[name="pass"]';
-export const leyesPrivado = 'https://testing.hlt.gob:3008';
-export const botonIngreso = 'button[role="btnLogin"]';
-export const botonMenuHamburguesa = '[data-testid="MenuIcon"]';
